1a38d34fa04fab358bff0c58ed8902ce836bddfe
tejas-kale <kaletejas2006@gmail.com>
2014-09-01 14:26:00 +0530

    Package source added

---
+++ R/astrowrap.R
@@ -0,0 +1,7 @@
+astrowrap <- function(test_name, ...) {
+  data(func_pack)
+  req_pack <- as.character(func_pack[func_pack[,1]==test_name, 2])
+  garb <- sapply(req_pack, FUN=function(x) suppressMessages(require(x, character.only=TRUE, quietly=TRUE, warn.conflicts=FALSE)))
+  args_list <- as.list(substitute(list(...)))[-1L]
+  do.call(test_name, args_list)
+}
