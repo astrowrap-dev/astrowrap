18ba4f0921af3b05d88f2461341c9e2c84b9d80b
tejas-kale <kaletejas2006@gmail.com>
2014-09-04 22:26:00 +0530

    Modified extension of data files

--- R/astrowrap.R
+++ R/astrowrap.R
@@ -1,5 +1,5 @@
 astrowrap <- function(test_name, ...) {
-  data(func_pack)
+  load(func_pack)
   req_pack <- as.character(func_pack[func_pack[,1]==test_name, 2])
   garb <- sapply(req_pack, FUN=function(x) suppressMessages(require(x, character.only=TRUE, quietly=TRUE, warn.conflicts=FALSE)))
   args_list <- as.list(substitute(list(...)))[-1L]
